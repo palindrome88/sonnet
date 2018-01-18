de765acfbe922fdd8d911604eb3baa69c25db2eb
Ryan Brisentine <brisentine.ryan@gmail.com>
2018-01-18 13:30:44 -0600

    first commit

---
+++ sonnet.js
@@ -0,0 +1,13 @@
+import { lchmod } from "fs";
+
+var longstring = document.getElementById("sonnet").innerHTML; //get all the contents of the id
+
+var word = "orphan"; // word you want to find
+
+var whereword = longstring.indexOf(word); // index of word
+
+var count = longstring.length; // length of entire word
+
+document.body.innerHTML = document.getElementById("sonnet").innerHTML.substr("winter", "yuletide");
+
+document.body.innerHTML = document.body.innerHTML.replace("the","a large");
