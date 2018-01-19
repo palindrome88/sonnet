d4c62ffe3d4870f11713a4801a1ed46f9d50c1e5
Ryan Brisentine <brisentine.ryan@gmail.com>
2018-01-19 11:36:14 -0600

    corrections

--- sonnet.js
+++ sonnet.js
@@ -1,13 +1,16 @@
-import { lchmod } from "fs";
+
 
 var longstring = document.getElementById("sonnet").innerHTML; //get all the contents of the id
 
+
 var word = "orphan"; // word you want to find
 
-var whereword = longstring.indexOf(word); // index of word
+var whereword = longstring.lastIndexOf(word); // index of word
 
 var count = longstring.length; // length of entire word
 
-document.body.innerHTML = document.getElementById("sonnet").innerHTML.substr("winter", "yuletide");
+var answer1= document.getElementById("sonnet").innerHTML.substr("winter", "yuletide");
 
-document.body.innerHTML = document.body.innerHTML.replace("the","a large");
+var answer2 = document.body.innerHTML.replace("the","a large");
+
+console.log(whereword);
